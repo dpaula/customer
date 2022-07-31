9e13d93700b1ece64bf51490e743c00042818801
Fernando de Lima <fernando.lima@ambevtech.com.br>
2022-07-31 19:27:53 -0300

    add infra k8s config, desabilitando eureka-server

--- src/main/java/com/dpaula/customer/client/fraud/IFraudClient.java
+++ src/main/java/com/dpaula/customer/client/fraud/IFraudClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 /**
  * @author Fernando de Lima on 29/07/22
  */
-@FeignClient("fraud")
+@FeignClient(name = "fraud", url = "${client.fraud.url}")
 public interface IFraudClient {
 
     @GetMapping("/api/v1/fraud-check/{customerId}")
